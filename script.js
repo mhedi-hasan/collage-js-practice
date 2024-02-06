18eff157b237995a8530912d458bf55c8c52e729
Mehedi Hasan <mhedih32@gmail.com>
2024-02-06 09:05:24 +0600

    script file uploded

---
+++ script.js
@@ -0,0 +1,32 @@
+// ---> Arithmetic Operators er use dekhano holo <---
+// let a = 10;
+// let b = 2;
+
+// console.log("a =",a,"& b =",b);
+// console.log("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a*b=", a * b);
+// console.log("a/b=", a / b);
+// console.log("a%b=", a % b);
+// console.log("a**b=", a ** b); // square kora holo  10*10=100
+// -----------------------------------
+
+//Unary Operator 2 ta:
+// 1/ Increment: er mane holo ++;
+// 2/ Decrement:er mane holo --;
+let c = 10;
+let d = 2;
+
+console.log("c =",c,"& d =",d);
+// d=d+1;
+// d++;
+// c=c-1;
+// c--;
+console.log('c =', c,"d =", d);
+console.log("c++",c++)//10
+console.log("c++",c++)//11
+console.log("++c",++c)//11
+// ----------------------
+console.log("d--",d--)//2
+console.log("d--",d--)//1
+console.log("--d",--d)//1
